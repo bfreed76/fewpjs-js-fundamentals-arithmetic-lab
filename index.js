b4e6e6d219f23db3b63e2c60deac67eeb3cc2046
Brinton <brintonfoyreed@gmail.com>
2021-04-05 17:59:20 -0700

    commit tree a5b4497904cdeb3e6df2fe82216713cde81872e5

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+const num1 = 31, num2 = 2;
+
+ const multiply = num1 * num2;
+
+
+const random = Math.round(Math.random()*10 +1);
+
+
+const num3 = 44;
+const num4 = 20;
+
+const mod = num3 % num4;
+
+
+const numz = (1, 3, 5, 20);
+
+const max = Math.max(numz);
+
